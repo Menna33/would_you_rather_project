de3df9a3435ddf6c78e8b60da4d4f5dbf8570333
Menna33 <mennalotfy33@gmail.com>
2021-05-24 01:43:37 +0200

    Add files via upload

---
+++ src/components/DashBoard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+class DashBoard extends React.Component {
+   
+
+render(){ 
+    return (
+
+<div></div>
+    )}}
+
+
+
+
+
+
+
+export default DashBoard
---
+++ src/components/NewQuestion.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+class NewQuestion extends React.Component {
+   
+
+render(){ 
+    return (
+
+<div></div>
+    )}}
+
+
+
+
+
+
+
+export default NewQuestion
