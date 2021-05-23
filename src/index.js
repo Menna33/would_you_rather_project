049e54c1b539548ed105b3457285879d11842482
Menna33 <mennalotfy33@gmail.com>
2021-05-23 21:15:13 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+//import store from './store';     //////////////???????????????????
+//0import router from './router';      //////////////???????????????????
+import { createStore } from 'redux'
+import reducer from './reducers'
+import middleware from './middleware'
+import App from './components/App'
+
+// Provider is a top-level component that wrapps our entire application, including
+// the Router. We pass it a reference to the store so we can use react-redux's
+// connect() method for Component Containers.
+const store = createStore(reducer,middleware)
+ReactDOM.render(
+  <Provider store={store}> <App /></Provider>,
+  document.getElementById('root')
+);
---
+++ src/utils/_DATA.js
@@ -0,0 +1,202 @@
+let users = {
+  sarahedo: {
+    id: 'sarahedo',
+    name: 'Sarah Edo',
+    avatarURL:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Favatarideas%2Ffemale-avatars%2F&psig=AOvVaw1IebVMbOBtUzuWIDOV02cj&ust=1621416141366000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKCY7f7z0vACFQAAAAAdAAAAABAD' ,
+    answers: {
+      "8xf0y6ziyjabvozdd253nd": 'optionOne',
+      "6ni6ok3ym7mf1p33lnez": 'optionTwo',
+      "am8ehyc8byjqgar0jgpub9": 'optionTwo',
+      "loxhs1bqm25b708cmbf3g": 'optionTwo'
+    },
+    questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
+  },
+  tylermcginnis: {
+    id: 'tylermcginnis',
+    name: 'Tyler McGinnis',
+    avatarURL: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fmale%2Bavatar&psig=AOvVaw0zZPNnIjqcvFVZs0T60PRL&ust=1621416187275000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiS1JT00vACFQAAAAAdAAAAABAD',
+    answers: {
+      "vthrdm985a262al8qx3do": 'optionOne',
+      "xj352vofupe1dqz9emx13r": 'optionTwo',
+    },
+    questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
+  },
+  johndoe: {
+    id: 'johndoe',
+    name: 'John Doe',
+    avatarURL:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shareicon.net%2Fyoung-man-user-avatar-male-person-103160&psig=AOvVaw0zZPNnIjqcvFVZs0T60PRL&ust=1621416187275000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiS1JT00vACFQAAAAAdAAAAABAJ' ,
+    answers: {
+      "xj352vofupe1dqz9emx13r": 'optionOne',
+      "vthrdm985a262al8qx3do": 'optionTwo',
+      "6ni6ok3ym7mf1p33lnez": 'optionTwo'
+    },
+    questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
+  }
+}
+
+let questions = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    author: 'sarahedo',
+    timestamp: 1467166872634,
+    optionOne: {
+      votes: ['sarahedo'],
+      text: 'have horrible short term memory',
+    },
+    optionTwo: {
+      votes: [],
+      text: 'have horrible long term memory'
+    }
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: '6ni6ok3ym7mf1p33lnez',
+    author: 'johndoe',
+    timestamp: 1468479767190,
+    optionOne: {
+      votes: [],
+      text: 'become a superhero',
+    },
+    optionTwo: {
+      votes: ['johndoe', 'sarahedo'],
+      text: 'become a supervillain'
+    }
+  },
+  "am8ehyc8byjqgar0jgpub9": {
+    id: 'am8ehyc8byjqgar0jgpub9',
+    author: 'sarahedo',
+    timestamp: 1488579767190,
+    optionOne: {
+      votes: [],
+      text: 'be telekinetic',
+    },
+    optionTwo: {
+      votes: ['sarahedo'],
+      text: 'be telepathic'
+    }
+  },
+  "loxhs1bqm25b708cmbf3g": {
+    id: 'loxhs1bqm25b708cmbf3g',
+    author: 'tylermcginnis',
+    timestamp: 1482579767190,
+    optionOne: {
+      votes: [],
+      text: 'be a front-end developer',
+    },
+    optionTwo: {
+      votes: ['sarahedo'],
+      text: 'be a back-end developer'
+    }
+  },
+  "vthrdm985a262al8qx3do": {
+    id: 'vthrdm985a262al8qx3do',
+    author: 'tylermcginnis',
+    timestamp: 1489579767190,
+    optionOne: {
+      votes: ['tylermcginnis'],
+      text: 'find $50 yourself',
+    },
+    optionTwo: {
+      votes: ['johndoe'],
+      text: 'have your best friend find $500'
+    }
+  },
+  "xj352vofupe1dqz9emx13r": {
+    id: 'xj352vofupe1dqz9emx13r',
+    author: 'johndoe',
+    timestamp: 1493579767190,
+    optionOne: {
+      votes: ['johndoe'],
+      text: 'write JavaScript',
+    },
+    optionTwo: {
+      votes: ['tylermcginnis'],
+      text: 'write Swift'
+    }
+  },
+}
+
+function generateUID () {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+export function _getUsers () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...users}), 1000)
+  })
+}
+
+export function _getQuestions () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...questions}), 1000)
+  })
+}
+
+function formatQuestion ({ optionOneText, optionTwoText, author }) {
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    }
+  }
+}
+
+export function _saveQuestion (question) {  //bt7ot el question fel array bta3t el questions
+  return new Promise((res, rej) => {
+    const authedUser = question.author;
+    const formattedQuestion = formatQuestion(question);
+
+    setTimeout(() => {
+      questions = {
+        ...questions,
+        [formattedQuestion.id]: formattedQuestion
+      }
+      
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          questions: users[authedUser].questions.concat([formattedQuestion.id])
+        }
+      }
+
+      res(formattedQuestion)
+    }, 1000)
+  })
+}
+
+export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          answers: {
+            ...users[authedUser].answers,
+            [qid]: answer
+          }
+        }
+      }
+
+      questions = {
+        ...questions,
+        [qid]: {
+          ...questions[qid],
+          [answer]: {
+            ...questions[qid][answer],
+            votes: questions[qid][answer].votes.concat([authedUser])
+          }
+        }
+      }
+
+      res()
+    }, 500)
+  })
+}
