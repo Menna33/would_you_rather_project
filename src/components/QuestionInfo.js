6ed89cd13cd3ba275ed4a32ae03695ee5b3cfa02
Menna33 <mennalotfy33@gmail.com>
2021-05-28 04:40:59 +0200

    Add files via upload

--- src/components/QuestionInfo.js
+++ src/components/QuestionInfo.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Redirect,withRouter } from 'react-router-dom'
 import '../styles/question.css'
 import { handleAnswerQuestion } from "../actions/questions.action";
+//import answerIcon from '../images/answer-icon.png'
 
 class QuestionInfo extends Component {
     state={
@@ -33,6 +34,7 @@ class QuestionInfo extends Component {
     const { question, author, authedUser, authedUserAnswer,optionOneVotes,optionTwoVotes,votesSum,optionOnePerecntage, optionTwoPerecntage } = this.props
     console.log('author',author)
     console.log('question',question)
+    console.log('authedUserAnswer:::::::::::::',authedUserAnswer)
     const answered=question.optionOne.votes.includes(authedUser)||question.optionTwo.votes.includes(authedUser)
     const optionOneText=question.optionOne.text
     console.log('question.optionOne.text: ',question.optionOne.text)
@@ -52,31 +54,30 @@ class QuestionInfo extends Component {
          {//check if it is answered or unanswered
          //if answered ->view votes and thier percantage
          answered ?(
-             <div>
+             <div className="container">
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
             
             <div className="answers-box">
-                {authedUserAnswer==="optionOne"&&(
-                    <div>
-                    <p>This is your answer</p>
-                    </div>
+                
+             <h3>--{question.optionOne.text}?</h3>
+             <div className="w3-container w3-blue w3-center" style={{width: optionOnePerecntage + "%"}}>{optionOnePerecntage}%</div>
+             {authedUserAnswer==="optionOne"&&(
+                    <h5 style={{color:"blue"}}>This is your answer</h5>
 
                 )
                 }
-             <p>{question.optionOne.text}?</p>
-             <div className="w3-container w3-blue w3-center" style={{width: optionOnePerecntage + "%"}}>{optionOnePerecntage}%</div>
              <p>{optionOneVotes} out of {votesSum}</p>
              </div>
              <div className="answers-box">
-                {authedUserAnswer==="optionTwo"?(
-                    <div>
-                    <p>This is your answer</p>
-                    </div>
-                ):(<div>
-                    </div>)
-                }
-             <p>{question.optionTwo.text}?</p>
+               
+             <h3>--{question.optionTwo.text}?</h3>
+             
              <div className="w3-container w3-blue w3-center"  style={{width: optionTwoPerecntage + "%"}}>{optionTwoPerecntage}%</div>
+             {authedUserAnswer==="optionTwo"&&(
+                   <h5 style={{color:"blue"}}>This is your answer</h5>
+
+                )
+                }
              <p>{optionTwoVotes} out of {votesSum}</p>
              </div>
              </div>
@@ -115,20 +116,13 @@ function mapStateToProps({ questions, users,authedUser }, { match}) {
         notExists,
     };}
   
-  console.log('id ',id)
-  console.log('match.params ',match.params)
-  console.log('match.params.questionID ',match.params.questionID)
-  console.log('Object.values(questions) ',Object.values(questions))
-  
-  console.log('question ',question)  ////////undefined why?
   const questionId = id
   const authorId = questions[id].author
   const author=users[authorId]
   console.log('author: ',author)
 
   //authedUser's answer
-  const authedUserAnswer=Object.keys(users[authedUser].answers);
-  console.log('authedUserAnswer: ',authedUserAnswer)
+  const authedUserAnswer=users[authedUser].answers[questionId];
 
   //perecntage of votes
   let optionOneVotes=question.optionOne.votes.length;
